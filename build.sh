9858eb0be40e9921718b9363f24d602a7426a33b
xunchangguo <xunchangguo@126.com>
2018-05-25 14:23:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 DEP_ROOT=$(git rev-parse --show-toplevel)
-mkdir -p "${DEP_ROOT}/release"
+mkdir -p "release"
+echo ${DEP_ROOT}
 GCR="gcr.io/google_containers/"
 cat gcr.io | while read line
 do
