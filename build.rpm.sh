38f6ba8199e3cfc8acb89645e254c04e16f10da4
xunchang.guo <xunchang.guo@chinacreator.com>
2018-06-13 15:14:16 +0800

    add rpm

---
+++ build.rpm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+    https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+cat k8s.gcr.io | while read line
+do
+  echo "build ${line} ......................."
+  yum install --downloadonly --downloaddir=release/ ${line}
+done
--- k8s.gcr.io
+++ k8s.gcr.io
@@ -1,2 +1,1 @@
-hyperkube:v1.7.12
 kubernetes-dashboard-amd64:v1.8.3
