2737c91e2c950dae0e7d5b32402b90cdd77d308d
xunchang.guo <xunchang.guo@chinacreator.com>
2018-06-15 09:21:30 +0800

    fix install

--- build.rpm.sh
+++ build.rpm.sh
@@ -4,5 +4,5 @@ basepath=$(pwd)
 cat k8s.rpm | while read line
 do
   echo "build ${line} ......................."
-  docker run --rm -v $basepath/kubernetes.repo:/etc/yum.repos.d/kubernetes.repo -v $basepath/release:/k8s  docker.io/centos:7 yum install --downloadonly --downloaddir=/k8s ${line}
+  docker run --rm -v $basepath/kubernetes.repo:/etc/yum.repos.d/kubernetes.repo -v $basepath/release:/k8s  docker.io/centos:7 yum install -y --downloadonly --downloaddir=/k8s ${line}
 done
