7a401afb6e23d3723c676de66f9c17d71dfa596d
xunchangguo <xunchangguo@gmail.com>
2020-11-18 09:07:15 +0800

    Update other_push.sh

--- other_push.sh
+++ other_push.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-cat other | while read line
-do
-  echo "pull ${line} ......................."
-  docker pull ${line}
-done
 echo "$SECRET_TKEY" | docker login -u xunchangguo --password-stdin docker.pkg.github.com
 cat other | while read line
 do
-  echo "push ${line} ......................."
+  echo "pull ${line} ......................."
+  docker pull ${line}  
+  echo "tag ${line} docker.pkg.github.com/xunchangguo/${line} ......"
   docker tag ${line} docker.pkg.github.com/xunchangguo/${line}
+  echo "push docker.pkg.github.com/xunchangguo/${line} ......"
   docker push docker.pkg.github.com/xunchangguo/repository-name/${line}
 done
