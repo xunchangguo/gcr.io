459256a7737f530fea4ccbdd8610a31de515bd13
xunchangguo <xunchangguo@gmail.com>
2021-04-06 12:31:14 +0800

    Update build.k8s.sh

--- build.k8s.sh
+++ build.k8s.sh
@@ -6,6 +6,6 @@ cat k8s.gcr.io | while read line
 do
   echo "build ${line} ......................."
   docker pull ${KGCR}${line}
-  docker save ${KGCR}${line} | gzip -c > release/${line}.tar
+  docker save ${KGCR}${line} | gzip -c > release/${line##*/}.tar
 done
 ls -l release/
